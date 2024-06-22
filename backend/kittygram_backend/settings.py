396d3244d19483da050f07e7c77bea266fe2bb4b
Козьмин Артём <kozmin_arti@mail.ru>
2024-06-22 09:11:37 +0700

    ревью

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,9 +4,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG', True))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'maannkkoo.ru', '0.0.0.0', '51.250.102.61', 'localhost']
 
@@ -99,7 +99,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
